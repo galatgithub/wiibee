5bce3671d90ae2e2d1ec10ee1cbe791414cac174
Guilhem A <guilhem.a@free.fr>
2022-01-25 16:50:00 +0000

    new autorun.sh version

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-date1=`date +%s`
-echo -e "$date1" >> timer.log
+#date1=`date +%s`
+#echo -e "$date1" >> timer.log
 
 # Bluetooth MAC, use: hcitool scan, or: python wiiboard.py
 # BTADDR="00:22:4c:6e:12:6c"
@@ -13,15 +13,18 @@ BTRLADDR="85:58:0E:16:65:F6"
 ## fix Huawei E3135 recognized as CDROM [sr0]
 #lsusb | grep 12d1:1f01 && sudo usb_modeswitch -v 0x12d1 -p 0x1f01 -M "55534243123456780000000000000a11062000000000000100000000000000"
 ## run DHCP client to get an IP
+
+# Affiche des informations sur toutes les interfaces réseau actives ou inactives sur le serveur | cherche ligne suivant ligne avec eth1 | cherche mot inet || (si different de 0)  renouvèle l'adresse IP de la carte réseau eth1  
 #ifconfig -a | grep eth1 -A1 | grep inet || sudo dhclient eth1
 #sleep 10
+
 #lsusb | grep 12d1:1f01 && sudo usb_modeswitch -v 0x12d1 -p 0x1f01 -M "55534243123456780000000000000a11062000000000000100000000000000"
 # run DHCP client to get an IP
 #ifconfig -a | grep eth1 -A1 | grep inet || sudo dhclient eth1
 #sleep 10
 
-# timer 1
-echo -e "1: ifconfig : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+## timer 1
+#echo -e "1: ifconfig : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 #sleep 12 # FIXME "wait" for dhcpd timeout
 # if BT failed: sudo systemctl status hciuart.service
@@ -44,8 +47,8 @@ until hciconfig hci0 up; do
     sleep 1
 done
 
-#timer 2 
-echo -e "2: until hciconfig : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 2 
+#echo -e "2: until hciconfig : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 logger "Simulate press red sync button on the Wii Board"
 
@@ -59,14 +62,13 @@ logger "Simulate press red sync button on the Wii Board"
 #echo -ne "pair $BTRLADDR" | bluetoothctl
 sudo rfcomm bind 0 $BTRLADDR
 
-#timer 2.1 
-echo -e "2.1: after rfcomm : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
-
+##timer 2.1 
+#echo -e "2.1: after rfcomm : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 sudo chmod o+rw /dev/rfcomm0
 
-#timer 2.2 
-echo -e "2.2: after chmod : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 2.2 
+#echo -e "2.2: after chmod : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 #ls -l /dev/rfcomm0
 
@@ -76,8 +78,8 @@ echo -e "\xA0\x01\x01\xA2" > /dev/rfcomm0 & pidbt=$!
 sleep 5
 kill -9 $pidbt 2>/dev/null
 
-#timer 2.3 
-echo -e "2.3: after open /sleep 5/ kill : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 2.3 
+#echo -e "2.3: after open /sleep 5/ kill : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 #switch OFF
 #echo -ne "\xA0\x01\x00\xA1" > /dev/rfcomm0 & pidbt=$!
@@ -86,41 +88,47 @@ echo -e "\xA0\x01\x00\xA1" > /dev/rfcomm0 & pidbt=$!
 sleep 10
 kill -9 $pidbt 2>/dev/null
 
-#timer 2.4 
-echo -e "2.4: after close /sleep 5 / kill: $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 2.4 
+#echo -e "2.4: after close /sleep 5 / kill: $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
-#timer 3
-echo -e "3: after switch on relay : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 3
+#echo -e "3: after switch on+off relay : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 logger "Start listening to the mass measurements"
 python autorun.py $BTADDR >> wiibee.txt
 
-#timer 4
-echo -e "4: after autorun.py : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 4
+#echo -e "4: after autorun.py : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 logger "Stopped listening"
 python txt2js.py wiibee < wiibee.txt > wiibee.js
 
-#timer 5
-echo -e "5: after txt2js.py wiibee : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 5
+#echo -e "5: after txt2js.py wiibee : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 python txt2js.py wiibee_battery < wiibee_battery.txt > wiibee_battery.js
 
-#timer 6
-echo -e "6: after txt2js.py wiibee_battery : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 6
+#echo -e "6: after txt2js.py wiibee_battery : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 git commit wiibee*.js -m"[data] $(date -Is)"
 git commit autorun.log -m"[data] $(date -Is)"
 
-#add commit timer.log
-git add timer.log
-git commit timer.log -m"[data] $(date -Is)"
+##add commit timer.log
+#git add timer.log
+#git commit timer.log -m"[data] $(date -Is)"
 
+# periodic push
+time2push=$(date -u +%H)
+ 
+if [ $(expr $time2push % 3) == "0" ]; then
 #git push origin master 2>A || cat A | mail -s "GIT a merdé sur Wiibee" guilhem.a@free.fr 
-git push origin master 2>A || cat A
+# 2> redirige erreur dans fichier A
+	git push origin master 2>A
+fi
 
-#timer 7
-echo -e "7: after git push : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+##timer 7
+#echo -e "7: after git push : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 # obexftp -b A0:CB:FD:F7:80:F1 -v -p wiibee.js
 # cp ~/wittyPi/wittyPi.log /mnt/bee1/
