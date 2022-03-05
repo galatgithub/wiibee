401091a7753e82101bac5441b90782e20fad2fb2
Guilhem A <guilhem.a@free.fr>
2022-03-05 16:47:53 +0000

    after update @ Fablab INSAT + wb low battery alert

--- autorun.sh
+++ autorun.sh
@@ -1,35 +1,25 @@
 #! /bin/bash
 
-#date1=`date +%s`
-#echo -e "$date1" >> timer.log
-
 # Bluetooth MAC, use: hcitool scan, or: python wiiboard.py
 # Balance installed : #2 Vert-jaune, var offset = -2.2 (à rajouter dans index.html) 
 BTADDR="00:1E:35:FD:11:FC"
 # Bluetooth relays addresses
 BTRLADDR="85:58:0E:16:7D:9C"
 
-# Connexion cle 3G
-## fix Huawei E3135 recognized as CDROM [sr0]
+## Connexion cle 3G
+## fix Huawei E33531 recognized as CDROM [sr0]
 #lsusb | grep 12d1:1f01 && sudo usb_modeswitch -v 0x12d1 -p 0x1f01 -M "55534243123456780000000000000a11062000000000000100000000000000"
 ## run DHCP client to get an IP
-
-# Affiche des informations sur toutes les interfaces réseau actives ou inactives sur le serveur | cherche ligne suivant ligne avec eth1 | cherche mot inet || (si different de 0)  renouvèle l'adresse IP de la carte réseau eth1  
 #ifconfig -a | grep eth1 -A1 | grep inet || sudo dhclient eth1
 #sleep 10
-
 #lsusb | grep 12d1:1f01 && sudo usb_modeswitch -v 0x12d1 -p 0x1f01 -M "55534243123456780000000000000a11062000000000000100000000000000"
-# run DHCP client to get an IP
+## run DHCP client to get an IP
 #ifconfig -a | grep eth1 -A1 | grep inet || sudo dhclient eth1
 #sleep 10
-
-## timer 1
-#echo -e "1: ifconfig : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
 #sleep 12 # FIXME "wait" for dhcpd timeout
 # if BT failed: sudo systemctl status hciuart.service
 hciconfig hci0 || hciattach /dev/serial1 bcm43xx 921600 noflow -
-
 # try /dev/ttyAMA0 or /dev/ttyS0 ?
 # try to install raspberrypi-sys-mods
 # try apt-get install --reinstall pi-bluetooth
@@ -47,12 +37,11 @@ until hciconfig hci0 up; do
     sleep 1
 done
 
-##timer 2 
-#echo -e "2: until hciconfig : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
-
 logger "Simulate press red sync button on the Wii Board"
 
 # Switch on bluetooth relay
+
+######## SINGLE WIIBOARD ###############################################
 
 #hcitool scan
 #echo -ne "scan on" | bluetoothctl
@@ -61,77 +50,147 @@ logger "Simulate press red sync button on the Wii Board"
 #echo -ne "trust $BTRLADDR" | bluetoothctl
 #echo -ne "pair $BTRLADDR" | bluetoothctl
 sudo rfcomm bind 0 $BTRLADDR
-
-##timer 2.1 
-#echo -e "2.1: after rfcomm : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
-
 sudo chmod o+rw /dev/rfcomm0
-
-##timer 2.2 
-#echo -e "2.2: after chmod : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
-
-#ls -l /dev/rfcomm0
-
 # switch ON
-#echo -ne "\xA0\x01\x01\xA2" > /dev/rfcomm0 & pidbt=$!
 echo -e "\xA0\x01\x01\xA2" > /dev/rfcomm0 & pidbt=$!
 sleep 5
 kill -9 $pidbt 2>/dev/null
 
-##timer 2.3 
-#echo -e "2.3: after open /sleep 5/ kill : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
-
 #switch OFF
-#echo -ne "\xA0\x01\x00\xA1" > /dev/rfcomm0 & pidbt=$!
-#sleep 5
 echo -e "\xA0\x01\x00\xA1" > /dev/rfcomm0 & pidbt=$!
 sleep 10
 kill -9 $pidbt 2>/dev/null
 
-##timer 2.4 
-#echo -e "2.4: after close /sleep 5 / kill: $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+#########################################################################
 
-##timer 3
-#echo -e "3: after switch on+off relay : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+######## MULTIPLE WIIBOARDs #############################################
 
-logger "Start listening to the mass measurements"
-python autorun.py $BTADDR >> wiibee.txt
+## Detection des relais
 
-##timer 4
-#echo -e "4: after autorun.py : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+###results=$(hcitool scan --numrsp=100)
+##results=$(hcitool -i hci0 scan | grep "JDY-30") 
+###echo $results
+##sleep 20
+##sudo systemctl restart bluetooth
 
-logger "Stopped listening"
-python txt2js.py wiibee < wiibee.txt > wiibee.js
+#nb_wiiboard=2
+#nb_counted=0
+#try=0
+#until [ $nb_counted -eq $nb_wiiboard -o $try -eq 10 ]; do
+    #((try++))
+    #results=$(hcitool -i hci0 scan | grep "JDY-30") 
+    #sleep 1
+    #nb_counted=$(echo $results | grep -o "JDY-30" | wc -l)
+    #echo $nb_counted
+    #[ $nb_counted -ne $nb_wiiboard ] && { echo "restart BT"; sudo systemctl restart bluetooth; sleep 10; }
+#done
 
-##timer 5
-#echo -e "5: after txt2js.py wiibee : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
 
-python txt2js.py wiibee_battery < wiibee_battery.txt > wiibee_battery.js
-
-##timer 6
-#echo -e "6: after txt2js.py wiibee_battery : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
-
-git commit wiibee*.js -m"[data] $(date -Is)"
-git commit autorun.log -m"[data] $(date -Is)"
-
-##add commit timer.log
-#git add timer.log
-#git commit timer.log -m"[data] $(date -Is)"
-
-# periodic push
-time2push=$(date -u +%H)
- 
-#if [ $(expr $time2push % 3) == "0" ]; then
-##git push origin master 2>A || cat A | mail -s "GIT a merdé sur Wiibee" guilhem.a@free.fr 
-## 2> redirige erreur dans fichier A
-	git push origin master 2>A
+#if [ $try -eq 10 ]; then
+    #echo "Problems : 10 attempts to restart bluetooth without response from all wiiboards, check wiiboards alimentation" | mail -s "Wiibee_clone1 : Problem with wiiboard" guilhem.a@free.fr
 #fi
 
-##timer 7
-#echo -e "7: after git push : $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)" >> timer.log
+#read -a strarr <<< "$results"
 
-# obexftp -b A0:CB:FD:F7:80:F1 -v -p wiibee.js
-# cp ~/wittyPi/wittyPi.log /mnt/bee1/
+#j=1
+#for i in $results; do
+	#if [ $((j++%2)) -eq 0 ]
+	#then
+	  #NAME+=("$i")
+	#else
+	  #MAC+=("$i")
+	#fi
+#done
+
+#BTRLADDR=""
+#j=0
+#for i in "${NAME[@]}"; do
+	#if [[ "$i" == *JDY-30* ]]
+	#then
+	  #BTRLADDR="$BTRLADDR ${MAC[$j]}"
+	#fi
+	#((j++))
+#done
+#BTRLADDR=${BTRLADDR:1}
+
+#echo "Relais detectes=${BTRLADDR[@]}"
+
+## Switch on/off des relais
+
+#N=0
+#LOGFILE=""
+#for nbtrl in $BTRLADDR; do
+    #FILE="/dev/rfcomm${N}"
+    #[ -f "$FILE" ] && { echo $(ls $FILE); } 
+    #[ ! -f "$FILE" ] && { echo "$FILE does not exist."; sudo rfcomm bind $N $nbtrl; sudo chmod o+rw /dev/rfcomm$N; }
+    #LOGFILE="$LOGFILE /dev/rfcomm$N"
+    #((N++)) 
+#done
+
+#LOGFILE=${LOGFILE:1}
+
+#open="\xA0\x01\x01\xA2"
+#for i in $LOGFILE; do
+    #echo "open $i"
+    #echo -e $open > "$i" & pidbt=$! &
+#done
+#sleep 5
+#kill $pidbt 2>/dev/null
+
+#close="\xA0\x01\x00\xA1"
+#for i in $LOGFILE; do
+    #echo "close $i"
+    #echo -e $close > "$i" & pidbt=$! &
+#done
+#sleep 5
+#kill $pidbt 2>/dev/null
+
+#((N--))
+#for i in `seq 0 $N`; do
+    #sudo rfcomm release $i
+#done
+
+#########################################################################
+
+logger "Start listening to the mass measurements"
+# replace python by python3
+python3 autorun.py $BTADDR >> wiibee.txt
+logger "Stopped listening"
+python txt2js.py wiibee < wiibee.txt > wiibee.js
+python txt2js.py wiibee_battery < wiibee_battery.txt > wiibee_battery.js
+
+# send alert if one of the wb < 4.5 volts
+flag_lowbat=($(awk -F " " 'END { for (i=2; i<=NF; i++) { print ($i<4.5) } }' wiibee_battery.txt))
+arr=($BTADDR)
+j=0
+for i in ${flag_lowbat[@]}; do
+    if [ $i -gt 0 ] 
+    then 
+        echo "Wiiboard ${arr[$j]} has low battery" | mail -s "Wiibee1 : Problem with wiiboard" guilhem.a@free.fr
+    fi
+    ((j++))
+done
+
+#cp ~/wittypi/schedule.log /mnt/bee1/wiibee/
+
+### git to github ##########################"
+
+######## old
+#git commit wiibee*.js -m"[data] $(date -Is)"
+#git commit autorun.log -m"[data] $(date -Is)"
+##git commit schedule.log -m"[data] $(date -Is)"
+##git push origin master 2>A || cat A | mail -s "GIT a merdé sur Wiibee" guilhem.a@free.fr
+#git push origin master 2>A
+########
+
+GIT=`which git`
+REPO_DIR=/mnt/bee1/wiibee/
+cd ${REPO_DIR}
+${GIT} commit wiibee*.js wb_temperatures.txt autorun.log -m "[data] $(date -Is)"
+${GIT} push origin master &>A
+#${GIT} push git@github.com:galatgithub/wiibee.git master 2>A
+
+#echo $WIIBEE_SHUTDOWN
 
 [ -z "$WIIBEE_SHUTDOWN" ] && exit 0
 logger "Shutdown WittyPi"
