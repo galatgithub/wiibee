ac5aa386269cd6dbc5ef09611519140f253c1a43
Guilhem A <guilhem.a@free.fr>
2022-03-05 16:47:08 +0000

    after updates @ Fablab INSAT

--- autorun.py
+++ autorun.py
@@ -65,13 +65,22 @@ wiiboards = [getwb(address) for address in sys.argv[1:]]
 # first connect then listen all balance, try to avoid connection refused error
 [wb.thread.start() for wb in wiiboards if wb]
 
+# xrange > range in python3
+def xrange(x):
+
+    return iter(range(x))
+
 for i in xrange(10):
     time.sleep(2)
     print("%.3f %.2f %.2f "%(time.time(), cpu_temp(), wtp_temp()) +
           " ".join(["%.2f"%(wb.average() if wb else 0) for wb in wiiboards]))
     battery_levels = " ".join(["%.2f"%(wb.battery if wb else 0) for wb in wiiboards])
+    wb_temperatures = " ".join(["%.2f"%(wb.temperature if wb else 0) for wb in wiiboards])
     
 with open("wiibee_battery.txt", "a+") as fp:
     fp.write(str(time.time()) + " " + battery_levels + "\n")
+
+with open("wb_temperatures.txt", "a+") as fp:
+    fp.write(str(time.time()) + " " + wb_temperatures + "\n")
 
 [wb.close() for wb in wiiboards if wb]
